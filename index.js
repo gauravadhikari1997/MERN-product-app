ffa4f2804d9e2d3b6b3c5918064e1890f6fedb7b
Gaurav Adhikari <39802561+gauravadhikari1997@users.noreply.github.com>
2020-06-25 12:50:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ app.use(bodyParser.json());
 //import routes
 require("./routes/productRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(port, () => {
   console.log(`Server is up and running on Port: ${port}`);
